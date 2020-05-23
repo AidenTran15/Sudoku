2f08fea2dbb3eaa439ca781e5d9560b34c24071d
AidenTran15 <aidenkiettran@gmail.com>
2020-05-23 16:04:19 +1000

    lastest update

--- origin.py
+++ origin.py
@@ -1,4 +1,4 @@
-#  GUI.py
+# GUI.py
 import pygame
 from solver import solve, valid
 import time
@@ -36,7 +36,7 @@ class Grid:
             self.cubes[row][col].set(val)
             self.update_model()
 
-            if valid(self.model, val, (row, col)) and solve(self.model):
+            if valid(self.model, val, (row,col)) and solve(self.model):
                 return True
             else:
                 self.cubes[row][col].set(0)
@@ -49,7 +49,7 @@ class Grid:
         self.cubes[row][col].set_temp(val)
 
     def draw(self, win):
-        #  Draw Grid Lines
+        # Draw Grid Lines
         gap = self.width / 9
         for i in range(self.rows+1):
             if i % 3 == 0 and i != 0:
@@ -59,7 +59,7 @@ class Grid:
             pygame.draw.line(win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
             pygame.draw.line(win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
 
-            # Draw Cubes
+        # Draw Cubes
         for i in range(self.rows):
             for j in range(self.cols):
                 self.cubes[i][j].draw(win)
@@ -84,7 +84,7 @@ class Grid:
         :return: (row, col)
         """
         if pos[0] < self.width and pos[1] < self.height:
-            gap = self.width /  9
+            gap = self.width / 9
             x = pos[0] // gap
             y = pos[1] // gap
             return (int(y),int(x))
@@ -103,7 +103,7 @@ class Cube:
     rows = 9
     cols = 9
 
-    def __init__(self, value, row, col, width, height):
+    def __init__(self, value, row, col, width ,height):
         self.value = value
         self.temp = 0
         self.row = row
@@ -123,11 +123,11 @@ class Cube:
             text = fnt.render(str(self.temp), 1, (128,128,128))
             win.blit(text, (x+5, y+5))
         elif not(self.value == 0):
-            text = fnt.render(str(self.value), 1, (0,0,0))
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
             win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
 
         if self.selected:
-            pygame.draw.rect(win, (255,0,0), (x,y, gap, gap), 3)
+            pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
 
     def set(self, val):
         self.value = val
@@ -138,15 +138,16 @@ class Cube:
 
 def redraw_window(win, board, time, strikes):
     win.fill((255,255,255))
-    #  Draw time
+    # Draw time
     fnt = pygame.font.SysFont("comicsans", 40)
     text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
     win.blit(text, (540 - 160, 560))
-    #  Draw Strikes
+    # Draw Strikes
     text = fnt.render("X " * strikes, 1, (255, 0, 0))
     win.blit(text, (20, 560))
-    #  Draw grid and board
+    # Draw grid and board
     board.draw(win)
+
 
 def format_time(secs):
     sec = secs%60
@@ -164,9 +165,8 @@ def main():
     key = None
     run = True
     start = time.time()
-    strikes = 0 
+    strikes = 0
     while run:
-
 
         play_time = round(time.time() - start)
 
@@ -206,7 +206,7 @@ def main():
                         key = None
 
                         if board.is_finished():
-                            print("Game Over")
+                            print("Game over")
                             run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -222,15 +222,5 @@ def main():
         redraw_window(win, board, play_time, strikes)
         pygame.display.update()
 
+
 main()
-pygame.quit()
-
-
-
-
-
-        
-
-
-
-
